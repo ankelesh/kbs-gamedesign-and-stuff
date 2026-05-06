222da4638351440a3f4504e91de254bf9e35686e
ankelesh <ankeleshbnet@inbox.ru>
2026-05-06 14:27:59 +0300

    added dwarven and demonic lore, fixed some inconsistensies

--- kbs/markdown/Фракции/Ростер юнитов/Ростер Чьеминь.mm
+++ kbs/markdown/Фракции/Ростер юнитов/Ростер Чьеминь.mm
@@ -1,22 +1,26 @@
-<map version="freeplane 1.11.5">
+<map version="freeplane 1.12.1">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
 <node TEXT="Ростер Чьеминь" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1754411428100" VGAP_QUANTITY="3 pt">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
-<hook NAME="MapStyle" background="#3c3836" zoom="1.3">
-    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/dark_gruvbox_template.mm" fit_to_viewport="false"/>
+<hook NAME="MapStyle" background="#3c3836ff" zoom="1.3">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/dark_gruvbox_template.mm" fit_to_viewport="false"/>
+    <tags category_separator="::"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="default" ID="ID_602083445" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="102.75 pt;-18 pt;" ENDINCLINATION="102.75 pt;3 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#a89984" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_602083445" STARTINCLINATION="103 pt;-17 pt;" ENDINCLINATION="103 pt;3 pt;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
 <edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
 <richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
 <richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.attributes">
 <font SIZE="9"/>
 </stylenode>
@@ -79,17 +83,17 @@
 <node TEXT="Нелетающая" ID="ID_1905481181" CREATED="1754411792255" MODIFIED="1754411813378"/>
 </node>
 <node TEXT="Юниты" ID="ID_233427586" CREATED="1754411689623" MODIFIED="1754411691479">
-<node TEXT="Рекрут МиньБинь (Народная стража)" ID="ID_1070551267" CREATED="1754411978329" MODIFIED="1754653800308">
-<node TEXT="Воин ДуньЦзы (путь Щита)" ID="ID_654417234" CREATED="1754412418725" MODIFIED="1754653809138">
-<node TEXT="Щитоносец ТьенБинь (Стража неба)" ID="ID_462051255" CREATED="1754412502981" MODIFIED="1754653823790">
-<node TEXT="Страж ЮньФа (варлорда)" ID="ID_792714488" CREATED="1754412765168" MODIFIED="1754653840749"/>
-<node TEXT="Страж ТьанТи (небесной лестницы)" ID="ID_989123874" CREATED="1754412839163" MODIFIED="1754653846423"/>
+<node TEXT="Рекрут БаоДзя 保甲 (Народная стража)" ID="ID_1070551267" CREATED="1754411978329" MODIFIED="1766654226522">
+<node TEXT="Воин ДуньБинь 盾兵  (путь Щита)" ID="ID_654417234" CREATED="1754412418725" MODIFIED="1766654239831">
+<node TEXT="Щитоносец ТьенБинь 天兵 (Стража неба)" ID="ID_462051255" CREATED="1754412502981" MODIFIED="1766654254012">
+<node TEXT="Страж ДзеДу 节度卫  (варлорда)" ID="ID_792714488" CREATED="1754412765168" MODIFIED="1766654339308"/>
+<node TEXT="Страж ТьанТи 天梯 (небесной лестницы)" ID="ID_989123874" CREATED="1754412839163" MODIFIED="1766654378747"/>
 </node>
 </node>
-<node TEXT="Воин ДаоЦзы (путь Меча" ID="ID_1322088480" CREATED="1754412856955" MODIFIED="1754653813863">
-<node TEXT="Патрульный БьенФанг (пограничник)" ID="ID_352461206" CREATED="1754412910014" MODIFIED="1754653831408">
-<node TEXT="Младший ЮньФа (варлорд)" ID="ID_292219299" CREATED="1754413031979" MODIFIED="1754653852990">
-<node TEXT="ЮньФа (варлорд)" ID="ID_598353592" CREATED="1754413055998" MODIFIED="1754653858476"/>
+<node TEXT="Воин ДаоБинь 刀兵 (путь Меча" ID="ID_1322088480" CREATED="1754412856955" MODIFIED="1766654410652">
+<node TEXT="Патрульный БьенДзю 边军 (пограничник)" ID="ID_352461206" CREATED="1754412910014" MODIFIED="1766654444710">
+<node TEXT="Младший ШаоДжу 少主 (варлорд)" ID="ID_292219299" CREATED="1754413031979" MODIFIED="1766654969746">
+<node TEXT="ДзеДуШи  节度使 (варлорд)" ID="ID_598353592" CREATED="1754413055998" MODIFIED="1766655007883"/>
 </node>
 </node>
 </node>
@@ -102,13 +106,13 @@
 <node TEXT="Арбалетчики" ID="ID_1081006504" CREATED="1754411820256" MODIFIED="1754411836381"/>
 </node>
 <node TEXT="Юниты" ID="ID_187534233" CREATED="1754411704744" MODIFIED="1754411723178">
-<node TEXT="Ополченец первой волны" ID="ID_774593301" CREATED="1754413083907" MODIFIED="1754413272438">
-<node TEXT="Стрелок ГонгЦзы (путь Лука)" ID="ID_735815769" CREATED="1754413273997" MODIFIED="1754653868956">
-<node TEXT="Лучник БаоТа (пагод)" ID="ID_1295837187" CREATED="1754413338052" MODIFIED="1754653882240">
-<node TEXT="Младший ТьеЮ (железный дождь)" ID="ID_497286283" CREATED="1754413444790" MODIFIED="1754653935702">
-<node TEXT="ТьенТье (метеорит)" ID="ID_1539960632" CREATED="1754413580656" MODIFIED="1754653953561"/>
+<node TEXT="Ополченец ЧьенФэн 前锋" ID="ID_774593301" CREATED="1754413083907" MODIFIED="1766655223490">
+<node TEXT="Стрелок ГонгБинь 弓兵 (путь Лука)" ID="ID_735815769" CREATED="1754413273997" MODIFIED="1766655257266">
+<node TEXT="Лучник БаоТа 宝塔 (пагод)" ID="ID_1295837187" CREATED="1754413338052" MODIFIED="1766655333300">
+<node TEXT="Стрелки ТьенЮ  天雨 (дождя)" ID="ID_497286283" CREATED="1754413444790" MODIFIED="1766655409933">
+<node TEXT="ТьенТье 天铁 (небесного железа)" ID="ID_1539960632" CREATED="1754413580656" MODIFIED="1766655437859"/>
 </node>
-<node TEXT="ТаШеШоу (башенный стрелок)" ID="ID_1896103522" CREATED="1754413656294" MODIFIED="1754653965534"/>
+<node TEXT="ТаНюШоу 塔弩手 (башенный стрелок)" ID="ID_1896103522" CREATED="1754413656294" MODIFIED="1766655534540"/>
 </node>
 </node>
 </node>
@@ -120,11 +124,11 @@
 <node TEXT="Призывающая" ID="ID_1832827368" CREATED="1754411863278" MODIFIED="1754411866498"/>
 </node>
 <node TEXT="Юниты" ID="ID_1688997514" CREATED="1754411708590" MODIFIED="1754433035321">
-<node TEXT="Каллиграфистка" ID="ID_78863549" CREATED="1754413721073" MODIFIED="1754413741870">
-<node TEXT="Ученица МоТа (волшебной башни)" ID="ID_1457036407" CREATED="1754413742398" MODIFIED="1754653973117">
-<node TEXT="Волшебница МоФэн (волшебного ветра)" ID="ID_1621724129" CREATED="1754432080481" MODIFIED="1754653982035">
-<node TEXT="Переписчица ВейЛайШу (книги будущего)" ID="ID_997322248" CREATED="1754432327437" MODIFIED="1754654003292">
-<node TEXT="Оракул ТьяЦзы (Тьи)" ID="ID_1056428611" CREATED="1754432390702" MODIFIED="1754654013976"/>
+<node TEXT="Каллиграфистка 墨生" ID="ID_78863549" CREATED="1754413721073" MODIFIED="1766655770715">
+<node TEXT="Ученица МоТа 墨塔 (волшебной башни)" ID="ID_1457036407" CREATED="1754413742398" MODIFIED="1766655782443">
+<node TEXT="Волшебница МоФэн  墨风  (волшебного ветра)" ID="ID_1621724129" CREATED="1754432080481" MODIFIED="1766655845170">
+<node TEXT="Переписчица ВейЛайШу 未来书 (книги будущего)" ID="ID_997322248" CREATED="1754432327437" MODIFIED="1766655862309">
+<node TEXT="Оракул ТьенМу 天母 (Тьи)" ID="ID_1056428611" CREATED="1754432390702" MODIFIED="1766656601482"/>
 </node>
 </node>
 <node TEXT="Призывательница ТьяФэн (ветра Тьи)" POSITION="bottom_or_right" ID="ID_365862807" CREATED="1754432528485" MODIFIED="1754653996012"/>
@@ -139,14 +143,14 @@
 </node>
 <node TEXT="Юниты" ID="ID_855023987" CREATED="1754411712415" MODIFIED="1754411731769">
 <node TEXT="Добытчица" ID="ID_414969403" CREATED="1754432650670" MODIFIED="1754432791977">
-<node TEXT="Остроглазка" ID="ID_920514900" CREATED="1754432793069" MODIFIED="1754432915438">
-<node TEXT="Охотница" ID="ID_1628952105" CREATED="1754432916006" MODIFIED="1754432933438">
-<node TEXT="Старшая охотница" ID="ID_1438932136" CREATED="1754432933898" MODIFIED="1754432938802">
-<node TEXT="Живой пример ЛьеРен (охотницы)" ID="ID_677076142" CREATED="1754433060142" MODIFIED="1754654096493"/>
+<node TEXT="Остроглазка 明眸" ID="ID_920514900" CREATED="1754432793069" MODIFIED="1766655975772">
+<node TEXT="Охотница 猎祝" ID="ID_1628952105" CREATED="1754432916006" MODIFIED="1766656010470">
+<node TEXT="Старшая охотница 猎师" ID="ID_1438932136" CREATED="1754432933898" MODIFIED="1766656049168">
+<node TEXT="Живой пример ЛьеРен 猎人真人(охотницы)" ID="ID_677076142" CREATED="1754433060142" MODIFIED="1766656165790"/>
 </node>
 </node>
-<node TEXT="Целительница ЮХэ (целитель)" ID="ID_1594293644" CREATED="1754432948288" MODIFIED="1754654035494">
-<node TEXT="Покровительница НонгМинь (народа)" ID="ID_473379290" CREATED="1754433223713" MODIFIED="1754654102735"/>
+<node TEXT="Целительница ЮХэ 雨荷 (целитель)" ID="ID_1594293644" CREATED="1754432948288" MODIFIED="1766656201307">
+<node TEXT="Покровительница ЙоуИ 游医" ID="ID_473379290" CREATED="1754433223713" MODIFIED="1766656248425"/>
 </node>
 </node>
 </node>
@@ -159,15 +163,15 @@
 <node TEXT="Лечение" ID="ID_1066963931" CREATED="1754433786042" MODIFIED="1754433789185"/>
 </node>
 <node TEXT="Юниты" ID="ID_839942596" CREATED="1754411716320" MODIFIED="1754411736872">
-<node TEXT="Ополченец тела вихря" ID="ID_62532774" CREATED="1754433320669" MODIFIED="1754433332753">
-<node TEXT="Воин ФанХуэй (возврата)" ID="ID_1368786787" CREATED="1754433334288" MODIFIED="1754654166630">
-<node TEXT="Мститель ТайЯнг (солнечный)" POSITION="bottom_or_right" ID="ID_146147122" CREATED="1754433458570" MODIFIED="1754654137482">
-<node TEXT="РенМиньХанВей (народный защитник)" ID="ID_209192535" CREATED="1754433522297" MODIFIED="1754654144844"/>
+<node TEXT="Ополченец тела вихря 涡卒" ID="ID_62532774" CREATED="1754433320669" MODIFIED="1766656314330">
+<node TEXT="Воин ХуэйЩен 回旋兵  (возврата)" ID="ID_1368786787" CREATED="1754433334288" MODIFIED="1766656475950">
+<node TEXT="Мститель ТайЯнг (солнечный) 太阳执法" POSITION="bottom_or_right" ID="ID_146147122" CREATED="1754433458570" MODIFIED="1766656507669">
+<node TEXT="ХуМинДзе 护民者 (народный защитник)" ID="ID_209192535" CREATED="1754433522297" MODIFIED="1766656558277"/>
 </node>
 </node>
-<node TEXT="Спаситель ТьенБанг (небесной помощи)" ID="ID_1801206348" CREATED="1754433695874" MODIFIED="1754654122392"/>
-<node TEXT="Метатель флешетт" POSITION="bottom_or_right" ID="ID_1906150369" CREATED="1754433604682" MODIFIED="1754433632112">
-<node TEXT="Сеятель ХуоШи (огненных камней)" ID="ID_575437731" CREATED="1754433632115" MODIFIED="1754654131110"/>
+<node TEXT="Спаситель ТьенДзю 天救 (небесной помощи)" ID="ID_1801206348" CREATED="1754433695874" MODIFIED="1766656434464"/>
+<node TEXT="Метатель флешетт 飞镖手" POSITION="bottom_or_right" ID="ID_1906150369" CREATED="1754433604682" MODIFIED="1766656344578">
+<node TEXT="Сеятель ХуоШи (огненных камней) 火雷" ID="ID_575437731" CREATED="1754433632115" MODIFIED="1766656396055"/>
 </node>
 </node>
 </node>
